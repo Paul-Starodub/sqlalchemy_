28f838cade6e208985369d685fd4f4fb65aaf8e2
Paul Starodub <amorallex@gmail.com>
2023-12-27 00:10:17 +0200

    update orm_metadata.py with classmethod

--- orm_metadata.py
+++ orm_metadata.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer
-from sqlalchemy.orm import as_declarative
+from sqlalchemy.orm import as_declarative, declared_attr
 
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
@@ -8,6 +8,11 @@ engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 @as_declarative()
 class AbstractModel:
     id = Column(Integer, autoincrement=True, primary_key=True)
+
+    @classmethod
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
 
 
 class UserModel(AbstractModel):
